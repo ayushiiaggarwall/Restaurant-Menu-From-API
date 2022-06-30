fbb6a9f9a2aa72e9db9d9336d5f1667b45b7d237
Ayushi Aggarwal <ayushiaggarwal1101@gmail.com>
2022-06-30 09:57:19 +0530

    changed alignment

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ $.ajax({
     url: 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian',
     success: function(response){
         for(var i=0; i<response.meals.length; i++){
-            var newItem = `<div class="col-3 my-3 mx-4 shadow-lg bg-body rounded text-center"> 
-            <h5 class="font-weight-bold my-2"> ${response.meals[i].strMeal} </h5> 
-            <img src = "${response.meals[i].strMealThumb}" class="img-fluid" /> 
-            <p class="my-2">${response.meals[i].idMeal}</p> 
+            var newItem = `<div class="col-3 my-3 mx-4 shadow-lg bg-body rounded"> 
+            <h5 class="font-weight-bold my-2 text-center"> ${response.meals[i].strMeal} </h5> 
+            <img src = "${response.meals[i].strMealThumb}" class="img-fluid text-center" /> 
+            <p class="my-2 text-end">Dish ID - ${response.meals[i].idMeal}</p> 
             </div>`
             $('#myitems').append(newItem);
         }
